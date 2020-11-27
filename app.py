aa25a0f968e53cdc4d4cce1c56740d4cee29cb74
EnderAsh <ashley3120@gmail.com>
2020-11-27 00:01:00 -0500

    dated_url_for only in dev

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import csv
 import os
 
 app = Flask(__name__)
-if os.environ['FLASK_ENV'] != 'development':
-    app.config['SERVER_NAME'] = 'acheung.me'
 
 cards = []
 with open("data.csv", encoding="utf-8-sig") as data:
@@ -16,20 +14,22 @@ for card in cards:
     card["Images"] = card["Images"].split("\n")
     card["Captions"] = card["Captions"].split("\n")
 
+if os.environ['FLASK_ENV'] != 'development':
+    app.config['SERVER_NAME'] = 'acheung.me'
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
+    @app.context_processor
+    def override_url_for():
+        return dict(url_for=dated_url_for)
 
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+    def dated_url_for(endpoint, **values):
+        if endpoint == 'static':
+            filename = values.get('filename', None)
+            if filename:
+                file_path = os.path.join(app.root_path,
+                                         endpoint, filename)
+                values['q'] = int(os.stat(file_path).st_mtime)
+        return url_for(endpoint, **values)
 
 
 @app.route('/')
